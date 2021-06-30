da7e4164830b6e743e74bdbc50611d1a1c3e8c06
Gamze Güneri <gamzegunerig@gmail.com>
2021-06-30 16:19:04 +0300

    adds .env

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
+
+
+const {REACT_APP_BACKEND_URL} =process.env;
 export default class BlogList extends Component {
   state={
     loading:true,
@@ -11,7 +14,7 @@ export default class BlogList extends Component {
 
   fetchAuthors = async () => {
     try {
-      const res = await fetch("http://localhost:3001/blogs")
+      const res = await fetch(REACT_APP_BACKEND_URL+"/blogs")
       if(res.ok){
         const data = await res.json()
         this.setState({blogs:data})
